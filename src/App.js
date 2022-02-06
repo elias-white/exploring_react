a2cf9c4113fe3bbb48f65fbf0ce38ca5aa6851fe
Elias White <git@eliaswhite.com>
2022-02-06 13:10:48 +0800

    More basic react tasks

--- src/App.js
+++ src/App.js
@@ -1,56 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Header = (props) => {
-    return <h1>{props.course}</h1>
-}
+const Title = ({text}) => <h1>{text}</h1>
 
-const Part = (props) => {
-    return <p> {props.part} {props.exercise} </p>
-}
+const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
 
-const Content = (props) => {
+const ActionBar = ({addGoodFeedback, addNeutralFeedback, addBadFeedback}) => {
     return (
         <>
-            <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
-            <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
-            <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
+            <Button text="Good" onClick={addGoodFeedback} />
+            <Button text="Neutral" onClick={addNeutralFeedback} />
+            <Button text="Bad" onClick={addBadFeedback} />
         </>
     )
 }
 
-const Total = (props) => {
+const Statistics = ({good, neutral, bad}) => {
+
+    if (good + neutral + bad === 0) {
+        return <p>No Feedback Given.</p>
+    }
     return (
-        <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
-        </p>
+        <>
+            <p>Good: {good}</p>
+            <p>Neutral: {neutral}</p>
+            <p>Bad: {bad}</p>
+            <p>All: {good + neutral + bad}</p>
+            # TODO Add Averages / Positive %
+            <p>Average: {}</p>
+            <p>Positive: {}%</p>
+        </>
     )
 }
 
-const App = () => {
 
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
-    }
+const App = () => {
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const addGoodFeedback = () => setGood(good + 1)
+    const addNeutralFeedback = () => setNeutral(neutral + 1)
+    const addBadFeedback = () => setBad(bad + 1)
 
     return (
-        <div>
-            <Header course={course.name} />
-            <Content parts={course.parts}/>
-            <Total parts={course.parts} />
-        </div>
+        <>
+            <Title text="Give Feedback" />
+            <ActionBar
+                addGoodFeedback={addGoodFeedback}
+                addNeutralFeedback={addNeutralFeedback}
+                addBadFeedback={addBadFeedback}
+            />
+            <Title text="Statistics" />
+            <Statistics good={good} neutral={neutral} bad={bad} />
+        </>
     );
 }
 
